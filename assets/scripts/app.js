ae5b6f75772d452127be23f03a58ed744a8e86e0
D-Raimon <raimon67@gmail.com>
2019-08-20 08:43:14 -0400

    Make style change to game board and button layout
    Made style changes in the index.scss in order to make the game look a
    bit more presentable. Also changed location of new game and get stats
    buttons in html so they are in a better spot for the player.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,5 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
 const gameEvents = require('./game/events.js')
 const authEvents = require('./auth/events.js')
 
